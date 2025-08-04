a6cc765589247a80033d9cb14222cebc8e01a4d9
Adil <115704572+Ash4477@users.noreply.github.com>
2025-08-04 21:00:34 +0500

    add auth redirect

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Login from "../components/Login/Login";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  const { isAuthenticated, loading } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated && !loading) navigate("/");
+  }, [isAuthenticated, loading, navigate]);
+
+  if (loading) return <p>Loading...</p>;
+  return <Login />;
 };
 
 export default LoginPage;
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Signup from "../components/Signup/Signup";
 
 const SignupPage = () => {
-  return (
-    <div>
-      <Signup />
-    </div>
-  );
+  const { isAuthenticated, loading } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated && !loading) navigate("/");
+  }, [isAuthenticated, loading, navigate]);
+
+  if (loading) return <p>Loading...</p>;
+  return <Signup />;
 };
 
 export default SignupPage;
