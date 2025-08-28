b596ac4b43068f2bc313c1b828d666f11c9bc0b9
Adil <115704572+Ash4477@users.noreply.github.com>
2025-08-28 15:16:40 +0500

    add server constants

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,6 @@
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const PAYPAL_CLIENT_ID = import.meta.env.VITE_PAYPAL_CLIENT_ID;
+const ENDPOINT = import.meta.env.VITE_ENDPOINT;
 
-export { SERVER_URL, BACKEND_URL };
+export { SERVER_URL, BACKEND_URL, PAYPAL_CLIENT_ID, ENDPOINT };
