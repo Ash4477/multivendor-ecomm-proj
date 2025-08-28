99cbcd53fe1e80d8473d05168d7dcf6dd0e0b8df
Adil <115704572+Ash4477@users.noreply.github.com>
2025-08-28 19:12:11 +0500

    update

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -2,8 +2,9 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 import catchAsyncErrors from "./catchAsyncErrors.js";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
+import Shop from "../models/shop.js";
 
-const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+const isUserAuthenticated = catchAsyncErrors(async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
@@ -17,4 +18,18 @@ const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
-export default isAuthenticated;
+const isShopAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { shop_token } = req.cookies;
+    if (!shop_token) {
+      return next(new ErrorHandler("Please login to continue.", 401));
+    }
+    const decoded = jwt.verify(shop_token, process.env.JWT_SECRET_KEY);
+    req.shop = await Shop.findById(decoded.id);
+    next();
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+export { isUserAuthenticated, isShopAuthenticated };
